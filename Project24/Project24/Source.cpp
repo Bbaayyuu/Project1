0259a6f7a687cf925ebbe0b916f01c9343b7ce0b
Bbaayyuu <marcgascon77@gmail.com>
2017-03-08 15:42:52 +0100

    New things
    kuk

--- Project24/Project24/Source.cpp
+++ Project24/Project24/Source.cpp
@@ -1,10 +1,11 @@
 #include<stdlib.h>
 #include"SDL/include/SDL.h"
+#include"SDL/include/SDL_image.h"
 
 
 #pragma comment (lib, "SDL/libx86/SDL2.lib")
 #pragma comment (lib, "SDL/libx86/SDL2main.lib")
-
+#pragma comment (lib, "SDL/libx86/SDL2_image.lib")
 #define MAX_BULLETS 40
 int main(int argc, char* args[] )
 {
@@ -21,9 +22,16 @@ int main(int argc, char* args[] )
 
 	//Creating the texture of the image from a surface.
 	SDL_Surface* image;
-	image = SDL_LoadBMP("Ship.bmp");
+	image = IMG_Load("Spaceship.png");
 	SDL_Texture* marc2 = SDL_CreateTextureFromSurface(Renderer, image);
 	SDL_FreeSurface(image);
+
+	//Texture for the enemies
+	SDL_Surface* image5;
+	image5 = SDL_LoadBMP("Enemy.bmp");
+	SDL_Texture* marc6 = SDL_CreateTextureFromSurface(Renderer, image5);
+	SDL_FreeSurface(image5);
+
 
 	//Creating the texture for the bullets.
 	SDL_Surface* image2;
@@ -37,12 +45,26 @@ int main(int argc, char* args[] )
 	SDL_Texture* marc4 = SDL_CreateTextureFromSurface(Renderer, image3);
 	SDL_FreeSurface(image3);
 
+	SDL_Surface* image4;
+	image4 = SDL_LoadBMP("Back.bmp");
+	SDL_Texture* marc5 = SDL_CreateTextureFromSurface(Renderer, image4);
+	SDL_FreeSurface(image4);
+
+
 	// Creating a rectangle
 	SDL_Rect r;
 	r.x = 100;
 	r.y = 100;
 	r.w = 80;
 	r.h = 80;
+
+	// Creating a rect for the enemies.
+	SDL_Rect r2;
+	r2.x = 1000;
+	r2.y = 200;
+	r2.w = 60;
+	r2.h = 60;
+
 
 	// Creating the shoots.
 	SDL_Rect s[MAX_BULLETS];
@@ -55,11 +77,19 @@ int main(int argc, char* args[] )
 		s[i].h = 20;
 	}
 	// Creating a background(provisional)
+	
 	SDL_Rect q;
 	q.x = 0;
 	q.y = 0;
-	q.w = 2580;
+	q.w = 1640;
 	q.h = 700;
+
+	SDL_Rect w;
+	w.x = 1640;
+	w.y = 0;
+	w.w = 1640;
+	w.h = 700;
+
 	
 	while(quit == false)
 	{
@@ -72,11 +102,33 @@ int main(int argc, char* args[] )
 		//To include the image in the size of the rectangle.
 		SDL_RenderDrawRect(Renderer, &r);
 		SDL_RenderFillRect(Renderer, &r);
+		SDL_RenderCopy(Renderer, marc5, NULL, &w);
 		SDL_RenderCopy(Renderer, marc4, NULL, &q);
 		SDL_RenderCopy(Renderer, marc2, NULL, &r);
+		SDL_RenderCopy(Renderer, marc6, NULL, &r2);
 		
 		//Movement of the background
+		
+		r2.x--;
+		/*if (r2.x == -50)
+		{
+			quit = true;
+			break;
+			SDL_Quit;
+		}*/
+
+		//To move the background.
 		q.x--;
+		w.x--;
+		if (q.x == -1640)
+		{
+			q.x = 1640;
+		}
+		if (w.x == -1640)
+		{
+			w.x = 1640;
+		}
+
 
 		for (int i = 0; i < MAX_BULLETS; i++)
 		{
